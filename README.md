43f7a7b15df363d8771f5223ca083e19c75a75fa
Erik Sunde <esu@esu.local>
2015-09-11 14:16:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Docker image for Enonic XP
+This Docker Compose set builds upon the Enonic XP image from `enonic/xp-app` and includes the superhero module from [https://github.com/enonic/module-superhero-blog]() downloaded from the official enonic repository. Do use another version of enonic XP, please change the baseimage tag in `Dockerfile`
+
+## Building the service
+To build the service, clone this repo
+```
+git clone https://github.com/enonic-io/docker-compose-enonic-xp-superhero
+cd docker-compose-enonic-xp-superhero
+```
+
+To configure the Instance automaticly, use the configure.sh script and replace "my-server.hostname.com" with your servers hostname. This will configure vhosts for both apache and Enonic XP.
+```
+./configure.sh my-server.hostname.com
+```
+
+Now, build the container set with docker-compose
+```
+dcker-composeo build 
+```
+
+## Running the application
+To run the application, simply run the following command after building the image as described above
+```
+$ docker-compose up -d 
+```
+
---
+++ configure.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+echo "###############################################################################"
+echo "### Enonic XP configurator"
+INSTANCE_HOSTNAME=$1
+
+if [[ "x$1" = "x" ]]
+	then
+	echo "hostname argument is missing, aborting."
+	exit 1
+fi
+
+APACHE2_VHOST_TEMPLATE=apache2/sites/vhost.example.conf.template
+EXP_VHOST_FILE=exp/config/com.enonic.xp.web.vhost.cfg
+
+
+echo "###############################################################################"
+echo "### Creating apache2 config for $INSTANCE_HOSTNAME"
+
+cp $APACHE2_VHOST_TEMPLATE apache2/sites/$INSTANCE_HOSTNAME.conf
+sed -i "s/SITE_HOSTNAME/$INSTANCE_HOSTNAME/g" apache2/sites/$INSTANCE_HOSTNAME.conf
+sed -i "s/SITE_HOSTNAME_ESCAPED/$(echo INSTANCE_HOSTNAME | sed 's/\./\\./g')/g" apache2/sites/$INSTANCE_HOSTNAME.conf
+
+
+echo "###############################################################################"
+echo "### Adding $INSTANCE_HOSTNAME to Enonic XP vhosts"
+
+sed -i "s/HOSTNAME/$MY_HOSTNAME/g" $EXP_VHOST_FILE
+
+echo "###############################################################################"
+echo "### Ready to build and deploy with docker-compose"
+echo "###############################################################################"
